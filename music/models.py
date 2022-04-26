5f903fc4f930747d5fd5b19f51c7c39aab5133f1
Dye <anthony.dye@ies-co.com>
2022-04-26 15:52:13 -0500

    Created Music Model for Music App + Registered Music and Rest Framework in Project Setting Installed App.

--- music/models.py
+++ music/models.py
@@ -1,3 +1,11 @@
+from xml.dom.minidom import AttributeList
 from django.db import models
 
 # Create your models here.
+
+class Music(models.Model):
+    title = models.CharField(max_length=255)
+    artist = models.CharField(max_length=255)
+    album = models.CharField(max_length=255)
+    release_date = models.DateTimeField()
+    genre = models.CharField(max_length=255)
