a3d9597bcf1d5ab5881090c5393f0f4a9126051c
Dye <anthony.dye@ies-co.com>
2022-04-26 16:12:43 -0500

    Updated Project URLS + Created First View Function + Created Music App urls File

--- music/views.py
+++ music/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+import imp
+import re
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+@api_view(['GET'])
+def music_list(request):
+    
+    return Response('ok')
+    
+    
